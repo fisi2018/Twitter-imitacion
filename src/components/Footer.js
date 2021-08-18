017cb82c815e3efbb400fa4b5c0d7a6b6549a320
Marco <marco.fura@unmsm.edu.pe>
2021-08-18 12:28:23 -0500

    first register interface finished

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import "./Footer.css";
+export const Footer=()=>{
+    return(
+        <footer className="container-footer" >
+            <a href="www.twitter.com">Acerca de</a>
+            <a href="www.twitter.com">Centro de ayuda</a>
+            <a href="www.twitter.com">Condiciones de servicio</a>
+            <a href="www.twitter.com">Políticas de privacidad</a>
+            <a href="www.twitter.com">Política de cookies</a>
+            <a href="www.twitter.com">Información de anuncios</a>
+            <a href="www.twitter.com">Blog</a>
+            <a href="">Estado</a>
+            <a href="">Empleos</a>
+            <a href="">Recursos para marcas</a>
+        </footer>
+    )
+}
